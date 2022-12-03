ceba7c5f8a8f04bd99fcb4788ee5af499adcc70d
mengneko00 <nekomemg05@gmail.com>
2022-12-03 15:07:49 +0800

    修改sum.js

--- sum.js
+++ sum.js
@@ -1,8 +1,12 @@
 function sum(n) {
-    // 回傳 1+2+3+4+...+n 的結果
-}
+    let j = 0
+    for (let i = 1; i <= n; i++) {
+        j += i
+    }
+    return j
+};
 
 console.log(sum(1)); // 1
 console.log(sum(2)); // 3
 console.log(sum(3)); // 6
-console.log(sum(10)); // 55
+console.log(sum(10)); // 55 
